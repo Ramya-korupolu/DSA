d6a29308866462bc6d1bcea3ab6bd79752b55492
Ramya Korupolu <104009248+Ramya-korupolu@users.noreply.github.com>
2023-03-13 11:38:41 +0530

    Create reverseBetween.java

---
+++ LinkedList/reverseBetween.java
@@ -0,0 +1,63 @@
+Given the head of a singly linked list and two integers left and right where left <= right, 
+reverse the nodes of the list from position left to position right, and return the reversed list.
+  
+  
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+
+
+
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left == right){
+            return head;
+        }
+
+        ListNode temp = head;
+        ListNode prev = null;
+
+        for(int i = 0; i < left - 1; i++){
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if(prev != null){
+            prev.next = reverseList(prev, temp, right - left + 1);
+        } else {
+            head = reverseList(prev, temp, right - left + 1);
+        }
+
+        return head;
+    }
+
+    
+
+    public ListNode reverseList(ListNode prev, ListNode node, int numbers){
+
+        if(node == null){
+            return node;
+        }
+
+        ListNode curr = node;
+        ListNode next = curr.next;
+        int count = 0;
+
+        while(count < numbers){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+
+        node.next = next;
+        return prev;
+
+
+    }
+}
